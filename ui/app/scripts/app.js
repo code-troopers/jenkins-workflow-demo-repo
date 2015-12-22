8b3e421da17cd891ae24a6bc413a3df3f4542cfa
Cedric Gatay <cedric@gatay.fr>
2015-12-22 13:35:47 +0100

    chore(init) : init of project
    Project init from restx scaffolder

---
+++ ui/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('jenkins-workflow-demoApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ ui/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jenkins-workflow-demoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'RESTX',
+      'Twitter Bootstrap',
+      'AngularJS',
+      'Karma'
+    ];
+  });
